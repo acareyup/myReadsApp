10d7dcf79c1e827722b915338d5a280ad308d90f
Eyup-Acar <acareyup@gmail.com>
2018-07-30 11:14:53 +0300

    chnage listbooks to stateless functional component

--- src/Book.js
+++ src/Book.js
@@ -16,7 +16,7 @@ class Book extends Component {
                             <li key={book.id} >
                                 <div className="book">
                                 <div className="book-top">
-                                    <div className="book-cover" key={book.id} style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks !== undefined ? book.imageLinks.thumbnail : ''})` }}></div>
+                                    <div className="book-cover" key={book.id} style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : ''})` }}></div>
                                     <div className="book-shelf-changer">
                                     <select value={book.shelf} onChange={(evt) => updateShelf(evt, book)} >
                                         <option value="move" disabled>Move to...</option>
--- src/listBooks.js
+++ src/listBooks.js
@@ -1,25 +1,19 @@
 import React, {Component} from 'react' 
 import Book from './Book'
 
-class ListBooks extends Component {
-
-    render(){
-        const {currentlyReading, wantToRead, read, updateShelf} = this.props
-        return(
-            <div className="list-books">
-                        <div className="list-books-title">
-                            <h1>MyReads</h1>
-                        </div>
-                        <div className="list-books-content">
-                            <Book updateShelf={updateShelf} books={currentlyReading} name={'Currently Reading'} />
-                            <Book updateShelf={updateShelf} books={wantToRead} name={'Want To Read'} />
-                            <Book updateShelf={updateShelf} books={read} name={'Read'} />
-                        </div>
-                    </div>
-        )
-         
-            
-    }
+const ListBooks = (props)=>{
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <Book updateShelf={props.updateShelf} books={props.currentlyReading} name={'Currently Reading'} />
+                <Book updateShelf={props.updateShelf} books={props.wantToRead} name={'Want To Read'} />
+                <Book updateShelf={props.updateShelf} books={props.read} name={'Read'} />
+            </div>
+        </div>
+    )
 }
 
 export default ListBooks
